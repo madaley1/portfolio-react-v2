2d5d6fb9c40bc789309b2310b2f43c09309ad960
madaley1 <mor2daley@gmail.com>
2023-07-31 08:10:47 -0700

    fix link style for disclaimer

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -28,8 +28,11 @@ export default function Index() {
           Please note, this portfolio is currently under construction. I&apos;m
           currently upgrading from React.js to Next.js, as well as transitioning
           to PostgreSQL, and many other cool tech upgrades! I appreciate your
-          patience, and feel free to check out the codebase in{' '}
-          <a href="https://github.com/madaley1/portfolio-react-v2">
+          patience, and feel free to check out the codebase in&nbsp;
+          <a
+            className="link"
+            href="https://github.com/madaley1/portfolio-react-v2"
+          >
             the GitHub!
           </a>
         </p>
