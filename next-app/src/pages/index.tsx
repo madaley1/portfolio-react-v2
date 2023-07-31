2c7e1aef73bc9aa67bfad8508c5e078d55591de1
madaley1 <mor2daley@gmail.com>
2023-07-31 08:26:14 -0700

    update language

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -25,10 +25,11 @@ export default function Index() {
           </div>
         </div>
         <p>
-          Please note, this portfolio is currently under construction. I&apos;m
-          currently upgrading from React.js to Next.js, as well as transitioning
-          to PostgreSQL, and many other cool tech upgrades! I appreciate your
-          patience, and feel free to check out the codebase in&nbsp;
+          Please note, this portfolio is currently under heavy construction.
+          I&apos;m currently upgrading from React.js to Next.js, as well as
+          transitioning to PostgreSQL, and many other cool tech upgrades! I
+          appreciate your patience, and feel free to check out the codebase
+          in&nbsp;
           <a
             className="link"
             href="https://github.com/madaley1/portfolio-react-v2"
